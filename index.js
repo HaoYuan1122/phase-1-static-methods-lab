6e7e4a3d4a153af7fc5e3c33cbc249180e0a3435
Hao Yuan <haomichaelyuan@gmail.com>
2023-03-20 22:48:34 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+//   add static methods here
+
+  static capitalize (string){
+     return string.charAt(0).toUpperCase()+string.slice(1)
+  }
+  static sanitize (string){
+    return string.replace(/[^a-zA-Z0-9-' /]+/g,"")
+  }
+  static titleize(string) {
+    const exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    let words = string.split(" ");
+    let titleizedWords = words.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return Formatter.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+    return titleizedWords.join(" ");
+  }
+
 }
+
